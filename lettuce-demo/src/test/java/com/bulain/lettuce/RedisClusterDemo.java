5e93dfb92a85c39aa46b03a489cc1e28c7c7da7c
Bulain Peng <bulain@gmail.com>
2018-05-29 23:05:47 +0800

    add redis cluster

--- lettuce-demo/src/test/java/com/bulain/lettuce/RedisClusterDemo.java
+++ lettuce-demo/src/test/java/com/bulain/lettuce/RedisClusterDemo.java
@@ -8,20 +8,20 @@ import org.junit.Test;
 
 import rx.Observable;
 
-import com.lambdaworks.redis.RedisClient;
 import com.lambdaworks.redis.RedisFuture;
-import com.lambdaworks.redis.api.StatefulRedisConnection;
 import com.lambdaworks.redis.api.async.RedisStringAsyncCommands;
 import com.lambdaworks.redis.api.rx.RedisStringReactiveCommands;
 import com.lambdaworks.redis.api.sync.RedisStringCommands;
+import com.lambdaworks.redis.cluster.RedisClusterClient;
+import com.lambdaworks.redis.cluster.api.StatefulRedisClusterConnection;
 
-public class RedisDemo {
+public class RedisClusterDemo {
 
-	RedisClient client = RedisClient.create("redis://localhost");
+	RedisClusterClient client = RedisClusterClient.create("redis://192.168.158.3:7381,192.168.158.3:7382,192.168.158.3:7383");
 
 	@Test
 	public void testSync() {
-		StatefulRedisConnection<String, String> connection = client.connect();
+		StatefulRedisClusterConnection<String, String> connection = client.connect();
 		RedisStringCommands<String, String> sync = connection.sync();
 		String set = sync.set("key", "value");
 		String value = sync.get("key");
@@ -32,7 +32,7 @@ public class RedisDemo {
 
 	@Test
 	public void testAsync() throws InterruptedException, ExecutionException {
-		StatefulRedisConnection<String, String> connection = client.connect();
+		StatefulRedisClusterConnection<String, String> connection = client.connect();
 		RedisStringAsyncCommands<String, String> async = connection.async();
 		RedisFuture<String> set = async.set("key", "value");
 		RedisFuture<String> get = async.get("key");
@@ -43,7 +43,7 @@ public class RedisDemo {
 
 	@Test
 	public void testReactive(){
-		StatefulRedisConnection<String, String> connection = client.connect();
+		StatefulRedisClusterConnection<String, String> connection = client.connect();
 		RedisStringReactiveCommands<String, String> reactive = connection.reactive();
 		Observable<String> set = reactive.set("key", "value");
 		Observable<String> get = reactive.get("key");
