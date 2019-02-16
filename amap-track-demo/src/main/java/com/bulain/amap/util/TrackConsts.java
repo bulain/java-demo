6e9111f819ff78e88d170caa39c1032332625703
Bulain Peng <bulain@gmail.com>
2019-02-16 18:42:26 +0800

    add track consts

---
+++ amap-track-demo/src/main/java/com/bulain/amap/util/TrackConsts.java
@@ -0,0 +1,31 @@
+package com.bulain.amap.util;
+
+public class TrackConsts {
+	
+	public static final String SERVICE_ADD = "https://tsapi.amap.com/v1/track/service/add";
+	public static final String SERVICE_DELETE = "https://tsapi.amap.com/v1/track/service/delete";
+	public static final String SERVICE_UPDATE = "https://tsapi.amap.com/v1/track/service/update";
+	public static final String SERVICE_LIST = "https://tsapi.amap.com/v1/track/service/list";
+	public static final String TERMINAL_ADD = "https://tsapi.amap.com/v1/track/terminal/add";
+	public static final String TERMINAL_DELETE = "https://tsapi.amap.com/v1/track/terminal/delete";
+	public static final String TERMINAL_UPDATE = "https://tsapi.amap.com/v1/track/terminal/update";
+	public static final String TERMINAL_LIST = "https://tsapi.amap.com/v1/track/terminal/list";
+	public static final String TERMINAL_SEARCH = "https://tsapi.amap.com/v1/track/terminal/search";
+	public static final String TERMINAL_AROUNDSEARCH = "https://tsapi.amap.com/v1/track/terminal/aroundsearch";
+	public static final String TERMINAL_POLYGONSEARCH = "https://tsapi.amap.com/v1/track/terminal/polygonsearch";
+	public static final String TERMINAL_DISTRICTSEARCH = "https://tsapi.amap.com/v1/track/terminal/districtsearch";
+	public static final String TRACE_ADD = "https://tsapi.amap.com/v1/track/trace/add";
+	public static final String TRACE_DELETE = "https://tsapi.amap.com/v1/track/trace/delete";
+	public static final String POINT_UPLOAD = "https://tsapi.amap.com/v1/track/point/upload";
+	public static final String TERMINAL_LASTPOINT = "https://tsapi.amap.com/v1/track/terminal/lastpoint";
+	public static final String TERMINAL_TRSEARCH = "https://tsapi.amap.com/v1/track/terminal/trsearch";
+	public static final String TERMINAL_COLUMN_ADD = "https://tsapi.amap.com/v1/track/terminal/column/add";
+	public static final String TERMINAL_COLUMN_DELETE = "https://tsapi.amap.com/v1/track/terminal/column/delete";
+	public static final String TERMINAL_COLUMN_UPDATE = "https://tsapi.amap.com/v1/track/terminal/column/update";
+	public static final String TERMINAL_COLUMN_LIST = "https://tsapi.amap.com/v1/track/terminal/column/list";
+	public static final String POINT_COLUMN_ADD = "https://tsapi.amap.com/v1/track/point/column/add";
+	public static final String POINT_COLUMN_DELETE = "https://tsapi.amap.com/v1/track/point/column/delete";
+	public static final String POINT_COLUMN_UPDATE = "https://tsapi.amap.com/v1/track/point/column/update";
+	public static final String POINT_COLUMN_LIST = "https://tsapi.amap.com/v1/track/point/column/list";
+
+}
