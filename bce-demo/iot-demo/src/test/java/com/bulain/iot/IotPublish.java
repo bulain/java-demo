4d69ca95b3a39c2bc7d74ae2b4cd206a32d07743
Bulain Peng <bulain@gmail.com>
2022-01-30 18:36:28 +0800

    升级spring-boot至版本2.6.3

--- bce-demo/iot-demo/src/test/java/com/bulain/iot/IotPublish.java
+++ bce-demo/iot-demo/src/test/java/com/bulain/iot/IotPublish.java
@@ -3,14 +3,11 @@ package com.bulain.iot;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = IotApplication.class)
 public class IotPublish {
 
