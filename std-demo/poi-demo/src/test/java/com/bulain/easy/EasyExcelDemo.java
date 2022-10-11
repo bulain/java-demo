f3e2e42809a10440aa09b4a88f98202cb7b84075
Bulain Peng <bulain@gmail.com>
2022-10-11 19:29:41 +0800

    excel动态导出示例

---
+++ std-demo/poi-demo/src/test/java/com/bulain/easy/EasyExcelDemo.java
@@ -0,0 +1,44 @@
+package com.bulain.easy;
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.util.ListUtils;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.List;
+
+public class EasyExcelDemo {
+
+    @Test
+    public void dynamicWrite() {
+        String fileName = "target/dynamicWrite-" + System.currentTimeMillis() + ".xlsx";
+        EasyExcel.write(fileName).head(head()).sheet("动态生成").doWrite(dataList());
+    }
+
+    private List<List<String>> head() {
+        List<List<String>> list = ListUtils.newArrayList();
+        List<String> head0 = ListUtils.newArrayList();
+        head0.add("字符串" + System.currentTimeMillis());
+        List<String> head1 = ListUtils.newArrayList();
+        head1.add("数字" + System.currentTimeMillis());
+        List<String> head2 = ListUtils.newArrayList();
+        head2.add("日期" + System.currentTimeMillis());
+        list.add(head0);
+        list.add(head1);
+        list.add(head2);
+        return list;
+    }
+
+    private List<List<Object>> dataList() {
+        List<List<Object>> list = ListUtils.newArrayList();
+        for (int i = 0; i < 10; i++) {
+            List<Object> data = ListUtils.newArrayList();
+            data.add("字符串" + i);
+            data.add(new Date());
+            data.add(0.56);
+            list.add(data);
+        }
+        return list;
+    }
+
+}
